0f289586d030d063ba86cc583326519a78d48d3e
Nara <leductienttkt@gmail.com>
2017-07-27 20:21:17 +0700

    aaaa

--- app/controllers/kho_controller.rb
+++ app/controllers/kho_controller.rb
@@ -2,6 +2,9 @@ class KhoController < ApplicationController
 
   def index
 
+  end
+
+  def show
     base_uri = "https://khotablet-abf64.firebaseio.com/"
     key = "qnGgiQepwP9jJLxZkeyFWloJ3dRqVTRTUuUXXbAL"
     firebase = Firebase::Client.new(base_uri, key)
@@ -9,7 +12,6 @@ class KhoController < ApplicationController
     @kho = response.body
     file_name = I18n.l(DateTime.now, format: :long).to_s
     respond_to do |format|
-      format.html
       format.xlsx do
         headers["Content-Disposition"] = "attachment; filename=\"#{file_name}.xlsx\""
         headers["Content-Type"] ||= Settings.xlsx
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   
   root "kho#index"
+  resources :kho
 end
